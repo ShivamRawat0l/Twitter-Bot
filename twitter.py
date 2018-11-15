145c4fba101e25f5a2c9c75c047b4e10fe322f60
Shivam Rawat <shivamrawat6670@gmail.com>
2018-11-15 20:05:01 +0530

    Twitter Robot
    It can right now just retweet the tweets having hash tag 12monthofproject. Currently ongoing process. Updates are coming.

---
+++ twitter.py
@@ -0,0 +1,19 @@
+import tweepy 
+f=open('password/password.txt','r')
+passwords=f.read().split('\n')
+
+consumer_key=str(passwords[0])
+consumer_secret=str(passwords[1])
+access_token=str(passwords[2])
+access_token_secret=str(passwords[3])
+print(consumer_key+"\n"+consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+tweets = api.search('#12monthsofcode')
+for tweet in tweets:
+    try:
+        api.retweet(tweet.id)
+    except:
+        print("Already Retweeted   ")
