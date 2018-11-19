cc19cdf098a8eb17c35ca3091cdf40f3fba811d5
Shivam Rawat <shivamrawat6670@gmail.com>
2018-11-19 17:48:14 +0530

    Commented the retweet feature and added the automatically tweeting feature

--- twitter.py
+++ twitter.py
@@ -11,9 +11,18 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-tweets = api.search('#12monthsofcode')
-for tweet in tweets:
-    try:
-        api.retweet(tweet.id)
-    except:
-        print("Already Retweeted   ")
+#tweets = api.search('#12monthsofcode')
+#for tweet in tweets:
+#    try:
+#        api.retweet(tweet.id)
+#    except:
+#        print("Already Retweeted   ")
+
+message= "Sometext"
+mentions=["shivam","rawat"]
+mention=""
+
+for i in mentions:
+    mention=mention+" @"+i
+print(message+mention)
+api.update_status(str(message)+" "+str(mention))
