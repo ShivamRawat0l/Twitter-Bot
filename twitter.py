732615cbe355b6ff20211b5dbc16f2522f57eb76
Shivam Rawat <shivamrawat6670@gmail.com>
2018-11-19 17:55:39 +0530

    Added comments

--- twitter.py
+++ twitter.py
@@ -11,12 +11,24 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+################################################
+#                                              #
+#   Automatically Retweet the Tweets           #
+#                                              #
+################################################
+
 #tweets = api.search('#12monthsofcode')
 #for tweet in tweets:
 #    try:
 #        api.retweet(tweet.id)
 #    except:
 #        print("Already Retweeted   ")
+
+################################################
+#                                              #
+#  Automatically Tweets message with mention   #
+#                                              #
+################################################
 
 message= "Sometext"
 mentions=["shivam","rawat"]
